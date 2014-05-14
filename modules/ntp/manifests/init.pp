a40d4ae9b3769f02f40caa3f9607ea11a3a349e5
Anselm Strauss <amsibamsi@gmail.com>
2014-05-14 22:26:36 +0200

    use single instead of double quotes

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,26 +1,29 @@
 class ntp(
-  $servers = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org", "3.pool.ntp.org"],
+  $servers = ["0.pool.ntp.org",
+              "1.pool.ntp.org",
+              "2.pool.ntp.org",
+              "3.pool.ntp.org"],
   $ntpdate_enable = false
 ) {
 
   file {
-    "/etc/ntp.conf":
-      content => template("ntp/ntp.conf.erb"),
-      notify => Service["ntpd"];
+    '/etc/ntp.conf':
+      content => template('ntp/ntp.conf.erb'),
+      notify => Service['ntpd'];
   }
 
   service {
-    "ntpd":
+    'ntpd':
       ensure => running,
       enable => true,
-      require => File["/etc/ntp.conf"];
+      require => File['/etc/ntp.conf'];
   }
 
   if $ntpdate_enable {
     service {
-      "ntpdate":
+      'ntpdate':
         enable => true,
-        require => File["/etc/ntp.conf"];
+        require => File['/etc/ntp.conf'];
     }
   }
 
